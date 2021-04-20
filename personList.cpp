e26fe5d1e9f0bd925366b761d443d250145b4dbd
rohanbehera03 <rohanbehera03@gmail.com>
2021-04-20 12:55:28 -0700

    Edit personList.h and personLists.cpp

--- personList.cpp
+++ personList.cpp
@@ -12,7 +12,9 @@ PersonList::PersonList(){
 }
 
 PersonList::~PersonList(){
+    for (unsigned int i = 0; i < numPeople; i++) { delete theList[i]; }
     delete [] theList;
+
 }
 
 void PersonList::addPerson(const char* child_name, const char* father_name, const char* mother_name){
